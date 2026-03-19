5e7ba237e82a7f16d1436905d3dc6e69cf270b22
Murillo <murillo.murillo2001@gmail.com>
2026-03-18 22:07:02 -0300

    Fixing main.ts

--- backend/apps/scheduling-service/src/main.ts
+++ backend/apps/scheduling-service/src/main.ts
@@ -3,7 +3,6 @@ import { SchedulingServiceModule } from './scheduling-service.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  /*
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     SchedulingServiceModule,
     {
@@ -18,23 +17,7 @@ async function bootstrap() {
     },
   );
 
-  await app.listen();*/
-
-  const app = await NestFactory.create(SchedulingServiceModule);
-
-  app.connectMicroservice({
-    transport: Transport.RMQ,
-    options: {
-      urls: ['amqp://localhost:5672'],
-      queue: 'appointments_queue',
-      queueOptions: {
-        durable: true,
-      },
-    },
-  });
-
-  await app.startAllMicroservices();
-  await app.listen(3000);
+  await app.listen();
 }
 
 bootstrap();
