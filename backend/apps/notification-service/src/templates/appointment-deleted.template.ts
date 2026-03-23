983caa3efe269665810af18dac69eed446519028
Murillo <murillo.murillo2001@gmail.com>
2026-03-23 15:49:05 -0300

    Removing unused imports

--- backend/apps/notification-service/src/templates/appointment-deleted.template.ts
+++ backend/apps/notification-service/src/templates/appointment-deleted.template.ts
@@ -1,5 +1,3 @@
-import { AppointmentDeletedEvent } from '@app/common/messaging/events/appointment-deleted.event';
-
 export function appointmentDeletedTemplate() {
   return {
     subject: 'Agendamento cancelado',
